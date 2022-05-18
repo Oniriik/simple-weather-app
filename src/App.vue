5f76929f27fa4c9c15631ca3c3a5bc2cdcf560ef
unknown <timot5924@gmail.com>
2022-05-18 12:52:14 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<p>{{this.city}}</p>
+<p>{{this.weather}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      apiKey: process.env.VUE_APP_API_KEY,
+      city: {
+        city: 'paris',
+        lat: '48.866667',
+        long :'2.333333'
+      },
+      weather: undefined
+    },
+  },
+  methods: {
+    getWeather(){
+      axios.get('https://api.openweathermap.org/data/3.0/onecall', {
+      params: {
+        lat: this.city.lat,
+        lon: this.city.long,
+        appid: this.apiKey
+      }
+    })
+    .then(res => {
+      console.log(res);
+      return res
+    })
+  },
+    }
+  },
+  beforeMount() {
+    this.getWeather().then((weather)=>{
+      this.weather = {... weather}
+    })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
