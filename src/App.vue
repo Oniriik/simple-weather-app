55a7ccd7323947c0d4f3defdd04fb825dca17850
unknown <timot5924@gmail.com>
2022-05-18 21:13:53 +0200

    added: search for a city's weather; refactor: getWeather

--- src/App.vue
+++ src/App.vue
@@ -9,33 +9,48 @@
       justify-center
       flex-col
       font-jakarta
-      duration-500
+      duration-1000
     "
   >
-    <div class="weather m-auto bg-black/70 w-96 h-auto rounded-3xl p-8">
+    <div
+      class="weather min-w-400 m-auto bg-black/70 w-auto h-auto rounded-3xl p-8"
+      v-if="initialized"
+    >
       <div class="search flex gap-3 items-center justify-center">
         <input
+          v-model="fieldCity"
           type="text"
-          name="city"
-          id="city"
           class="rounded-3xl w-40 pl-3 pr-3 pt-1 pb-1 outline-0"
+          @keyup.enter="updateWeather(fieldCity)"
         />
-        <img src="@/assets/search.png" alt="search" class="w-4 h-4 invert" />
+        <img
+          src="@/assets/search.png"
+          alt="search"
+          class="w-4 h-4 invert cursor-pointer"
+          @click="updateWeather(fieldCity)"
+        />
       </div>
       <div
-        class="display-weather mt-4 text-white flex flex-row justify-between"
+        class="
+          display-weather
+          mt-4
+          text-white
+          flex flex-row
+          gap-5
+          justify-between
+        "
       >
         <div>
           <p class="text-xl">Weather in</p>
-          <p class="capitalize text-5xl">{{ city.name }}</p>
+          <p class="capitalize text-5xl">{{ weather?.name }}</p>
           <div class="text-xs mt-7">
-            <p>Feels {{ Math.round(weather.main.feels_like) }}°</p>
-            <p>Humidity {{weather.main.humidity}}%</p>
-            <p>Wind {{weather.wind.speed}} km/h</p>
+            <p>Feels {{ Math.round(weather.main?.feels_like) }}°</p>
+            <p>Humidity {{ weather.main?.humidity }}%</p>
+            <p>Wind {{ weather.wind?.speed }} km/h</p>
           </div>
         </div>
-        <div>
-          <p class="text-7xl mt-2">{{ Math.round(weather.main.temp) }}°</p>
+        <div class="text-right">
+          <p class="text-7xl mt-2">{{ Math.round(weather.main?.temp) }}°</p>
           <img :src="weather.weatherIcon" alt="weatherIcon" />
         </div>
       </div>
@@ -49,13 +64,9 @@ export default {
   components: {},
   data() {
     return {
+      initialized: false,
       apiKey: process.env.VUE_APP_WEATHER_API_KEY,
       fieldCity: "",
-      city: {
-        name: "paris",
-        lat: "48.866667",
-        long: "2.333333",
-      },
       weather: {},
       background: {
         orientation: "landscape",
@@ -64,38 +75,44 @@ export default {
   },
   methods: {
     getWeather(city) {
+      console.log(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+      );
       axios
         .get(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
         )
         .then((res) => {
-          console.log(res);
           this.weather = { ...res.data };
           this.weather.weatherIcon = `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
-          console.log(
-            `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+
+          fetch(`https://source.unsplash.com/1600x900/?${this.weather.weather[0].description}`).then(
+            (response) => {
+              document.getElementById(
+                "page-container"
+              ).style.backgroundImage = `url('${response.url}')`;
+            }
           );
         })
         .catch((err) => {
           console.error(err);
         });
+      return;
     },
-    getBackgroundImg() {
-      var query = encodeURI(
-        `${this.weather.current_weather} ${this.weather.time}`
-      );
-      fetch(`https://source.unsplash.com/1600x900/?${query}`).then(
-        (response) => {
-          document.getElementById(
-            "page-container"
-          ).style.backgroundImage = `url('${response.url}')`;
-        }
-      );
+    updateWeather(city) {
+      this.getWeather(city);
+      this.fieldCity = "";
+    },
+    initialize() {
+      this.getWeather("paris");
+      setTimeout(()=>{
+        this.initialized = true;
+      },500)
+      
     },
   },
-  async beforeMount() {
-    this.getWeather(this.city.name);
-    this.getBackgroundImg();
+  created() {
+    this.initialize();
   },
 };
 </script>
